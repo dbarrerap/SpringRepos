88f37bea51e77aa0d2ec698e315cd76ea1fa051f
David E. Barrera <dbarrerap@live.com>
2022-01-07 20:08:09 -0500

    Initial commit

---
+++ src/test/java/io/github/dbarrerap/photorepos/PhotoReposApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.dbarrerap.photorepos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PhotoReposApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
